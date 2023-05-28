2a537c81f76987fa37cd479996281088ef4370b7
tomide <tomideadeyanju@gmail.com>
2023-05-28 05:50:28 +0100

    changes made

--- stylist-Landing-page/src/Components/About/About.jsx
+++ stylist-Landing-page/src/Components/About/About.jsx
@@ -22,7 +22,7 @@ const About = () => {
         </button>
       </div>
      
-      <svg className=" absolute 2xl:mt-[450px] xs:mt-[700px]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+      <svg className=" absolute 2xl:mt-[450px] xs:mt-[660px]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
           <path
             fill="#fff"
             fill-opacity="1"
--- stylist-Landing-page/src/Components/Approach/Approach.jsx
+++ stylist-Landing-page/src/Components/Approach/Approach.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 
 const Approach = () => {
   return (
-    <div>
+    <div className='xs:z-10  '>
       <div>
-        <h1 className='text-center font-extrabold text-2xl  bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500'>About Us</h1>
+        <h1 className='text-center font-extrabold text-2xl   bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500'>About Us</h1>
+     
+     <p className='text-center font-bold xs:text-xs'>Seaplace is committed to bringing captivating ideas of modern NFT. <br />we join forces with collectors and artists and blockchain technology </p>
       </div>
     </div>
   )
